904c050c6e6a42b09f2b68339b3d97209674bf81
ramzi0001 <menouerramzi0001@gmail.com>
2024-08-07 01:12:33 +0200

    database

---
+++ database/migrations/2024_08_05_230931_create_store_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('store_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('store_id')->constrained('shops')->onDelete('cascade');
+            $table->integer('points');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('store_user');
+    }
+};
