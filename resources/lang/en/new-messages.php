68b698e0e18d8c628bb256f77377e8c781647caa
ramzi0001 <menouerramzi0001@gmail.com>
2024-08-17 20:03:36 +0100

    no

--- resources/lang/en/new-messages.php
+++ resources/lang/en/new-messages.php
@@ -8358,4 +8358,6 @@ return [
 	"removed_from_wishlist" => "Removed from wishlist",
 	"you_get_free_delivery_bonus" => "You get free delivery bonus",
 	"you_have_not_add_any_products_in_Wishlist " => "You have not add any products in Wishlist ",
+	"We_are_sorry_server_is_not_responding" => "We are sorry server is not responding",
+	"Try_after_sometime" => "Try after sometime",
 ];
