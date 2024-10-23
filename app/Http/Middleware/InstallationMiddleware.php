e18230c730396bc74a01756299ab81281fa09d25
mehdi harzallah <96887412+opestro@users.noreply.github.com>
2024-10-24 00:16:09 +0100

    Update InstallationMiddleware.php

--- app/Http/Middleware/InstallationMiddleware.php
+++ app/Http/Middleware/InstallationMiddleware.php
@@ -15,12 +15,12 @@ class InstallationMiddleware
      */
     public function handle($request, Closure $next)
     {
-        // if (session()->has('purchase_key') == false && env('PURCHASE_CODE') == null) {
-        //     session()->flash('error', base64_decode('SW52YWxpZCBwdXJjaGFzZSBjb2RlIGZvciB0aGlzIHNvZnR3YXJlLg=='));
-        //     return redirect('step2');
-        // }elseif(env('PURCHASE_CODE') != null){
-        //     return $next($request);
-        // }
+         if (session()->has('purchase_key') == false && env('PURCHASE_CODE') == null) {
+             session()->flash('error', base64_decode('SW52YWxpZCBwdXJjaGFzZSBjb2RlIGZvciB0aGlzIHNvZnR3YXJlLg=='));
+             return redirect('step2');
+         }elseif(env('PURCHASE_CODE') != null){
+             return $next($request);
+         }
 
         return $next($request);
     }
